a7ac2138c26f0ca19fffa4b6a8c33cabc2de06ae
Aryan <aryanmaurya1@outlook.com>
2021-05-31 18:19:34 +0530

    Updated README.md and added instructions for deploying.

--- README.md
+++ README.md
@@ -1,4 +1,65 @@
-# liveLambda
+# LiveLambda
 A libray for building backends using AWS Lambda.
 
 ## Make Your Servers Serverless 🤓
+
+### Instructions
+* liveLambda_v2.py and you ``<source_file>`` should be in same directory.
+* Import ``LiveLambdaV2`` class from ``liveLambda_v2.py`` file.
+### Example
+
+```
+from liveLambda_v2 import LiveLambdaV2
+
+app = LiveLambdaV2()
+
+# Route with a single method attached to it.
+
+@app.route("/", "GET")
+def getIndex():
+  return {"page" : str(app.request), "route" : "GET /"}
+
+@app.route("/", "POST")
+def postIndex():
+  return {"page" : str(app.request), "route" : "POST /"}
+
+# Route with url parameters.
+
+@app.route("/user/{id}", "POST")
+def f3(id):
+  return {"page" : str(app.request), "route" : "POST /user/{id}"}
+
+
+# Route with multiple methods attached to it.
+
+@app.route("/dynamic", ["GET", "POST", "PUT", "PATCH", "DELETE"])
+def f4():
+  return {"key" : str(app.request), "route" : "GET /user"}
+
+# Your source file must containe a method which lambda runtime will invoke.
+# Name of function must be same in deployment config file.
+
+def lambda_handler(event, context):
+  response = app.run(event, context)
+  return response
+
+```
+
+## Deployment
+
+### Instructions
+* Your source code must contain a ``config.json`` file which will contains deployment configuration and 'arn' of the role used by the AWS lambda to push the logs at AWS cloudwatch.
+
+* Make a single `.zip` file containing` liveLambda_v2.py`, `<your_source_code>.py`, and `all_libraries_you_use(files and folders)` .
+### Sample ``config.json``
+```
+{
+  "file_path": "<path_to_zip_file>",
+  "lambda_role_arn": "<arn>",
+  "function_name": "<lambda_function_name>",
+  "handler_function": "<source_file>.<starting_function_name>",
+  "api_name": "<cloudwatch_api_name>"
+}
+```
+* Run ``deploy.py`` file. 
+* your ``config.json`` and ``deploy.py`` should be in same folder.
--- liveLambda_v2.py
+++ liveLambda_v2.py
@@ -1,6 +1,10 @@
 import json
 
 class Request:
+  # Base class for a request object. Every request object comming from API gateway is transformed internally into 
+  # LiveLambdaV2's internal request object. 
+  # REQUEST (API GATEWAY) --> REQUEST (LiveLambda)
+  
   def __init__(self):
       self.request_context = {}
       self.headers = {}
@@ -23,6 +27,9 @@ class Response:
     return {"body" : self.body, "header": self.headers}
 
 class LiveLambdaV2:
+  # Main class which handles the flow from registering handler functions to calling them on correct routes.
+  # Evey LiveLambdaV2 webapp must contain atleast one object of this class.
+
   def __init__(self):
     self.__route_dict = { "POST" : {}, "GET" : {}, "PUT" : {},"DELETE" : {} ,"ANY" : {}, "PATCH" : {} }
     self.__app_config = {}
@@ -31,12 +38,25 @@ class LiveLambdaV2:
     self.response = {}
   
   def register_route(self, route, methods, handler):
-    # print(route," " * (50 - len(str(route))), methods, " " * (40 - len(str(methods))) ,handler)
+    # Internal method, should not be used directly. Instead use @app.route() decorator to register handler functions.
+    # Here 'app' refers to a LiveLambdaV2 Object.
+    # @route:string -> Endpoint  for which to register handler. 
+    # @methods:list -> List of methods for which hanlder should be registered to a given route.
+    # @handler:function -> Handler function to register.
+    
     for method in methods:
       self.__route_dict[method][route] = handler
 
 
   def route(self, route, method):
+    # This is a decorator, usage is similar like route decorator in flask.
+    # usage : @app.route(route='/{id}', method=['GET'])
+    #         def home(id):
+    #           pass
+    # Here 'app' is a LiveLambda object.
+    # @route:string -> Endpoint  for which to register handler. 
+    # @method:list/string ->  List of methods for which hanlder should be registered to a given route.
+
     isMethodList = type(method) == type([])
     if not isMethodList:
       # If there is a single method, then converting it into list of method.
@@ -48,6 +68,8 @@ class LiveLambdaV2:
     return wrapper
 
   def generate_route_keys(self):
+    # Returns a list of all routes registered with an particular LiveLambdaV2 object.
+
     full_paths = []
     for method in self.__route_dict.keys():
       for route in self.__route_dict[method]:
@@ -57,6 +79,9 @@ class LiveLambdaV2:
 
 
   def get_param_name(self, route):
+    # Internal method, should not be used directly.
+    # run method internally uses it to find out the name of url parameter in a registered route.
+
     start = None
     end = None
     for i in range(len(route)):
@@ -68,6 +93,12 @@ class LiveLambdaV2:
     return route[start+1 : end]
 
   def run(self, event, context):
+    # This method takes an event and context object provided by the AWS Lambda handler function and calls 
+    # appropriate handler internally and returns the proper response.
+    # event:dict -> Event dictionary which lambda_handler function (first function which AWS Lambda runtime calls) 
+    #     received as an argument.
+    # context:object -> Context Object which lambda_handler function (first function which AWS Lambda runtime calls) 
+    #     received as an argument.
 
     self.request = Request()
     self.response = Response()
